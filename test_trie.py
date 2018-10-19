a379d5afca97eb55f1eb99dc2edd4521a7e0d70e
rudy <rudy@shypwright.com>
2018-10-18 22:23:01 -0700

    Adding trie code

---
+++ test_trie.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+
+class Node():
+
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+        self.term = False
+
+
+def get_words():
+    return [
+        "vacation",
+        "valley",
+        "valuable",
+        "variety",
+        "vast",
+        "version",
+        "victim",
+        "video",
+        "view",
+        "violence",
+    ]
+
+
+def make_trie(words):
+
+    root = Node(None)
+
+    for word in words:
+        current = root
+
+        for letter in list(word):
+
+            for child in current.children:
+                if child.value == letter:
+                    current = child
+                    break
+
+            node = Node(letter)
+            current.children.append(node)
+            current = node
+
+        current.term = True
+
+    return root
+
+
+def search_trie(word, root):
+    current = root
+    total = len(word)
+    matched = 0
+    for c in list(word):
+        found = False
+        for child in current.children:
+            if c == child.value:
+                matched += 1
+                if matched == total and child.term == True:
+                    return True
+                current = child
+                break
+
+    return False
+
+
+def test_trie_found():
+    word = "vacation"
+    root = make_trie(get_words())
+    actual = search_trie(word, root)
+    expected = True
+    assert actual == expected
+
+
+def test_trie_not_found_almost():
+    word = "vacatio"
+    root = make_trie(get_words())
+    actual = search_trie(word, root)
+    expected = False
+    assert actual == expected
+
+
+def test_trie_not_found_longer_word():
+    word = "videographer"
+    root = make_trie(get_words())
+    actual = search_trie(word, root)
+    expected = False
+    assert actual == expected
+
+
+def test_trie_not_small_word():
+    word = "vim"
+    root = make_trie(get_words())
+    actual = search_trie(word, root)
+    expected = False
+    assert actual == expected
